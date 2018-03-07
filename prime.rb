0721ef188a51806ee82b4d09f18290da1439adb5
Kelsey Fecho <kfecho30@gmail.com>
2018-03-07 21:50:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,29 +1,13 @@
-#def prime?(n)
-#  test = true
-#  if n < 2
-#    test = false
-#  else
-#    for i in 2..(n-1)
-#      if n%i == 0
-#        test = false
-#      end
-#    end
-#  end
-#  test
-#end
-
-def prime?(num)
-  if num <= 1
-    return false
+def prime?(n)
+  test = true
+  if n < 2
+    test = false
   else
-    x_array = (2...num).to_a
-    is_it = x_array.map do |x|
-      num % x == 0
-    end
-    if is_it.include?(true)
-      return false
-    else
-      return true
+    for i in 2..(n-1)
+      if n%i == 0
+        test = false
+      end
     end
   end
+  test
 end
